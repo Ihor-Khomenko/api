be314d095a80ddde55cb9f713629f08a6d3a0c1a
Ihor Khomenko <39989281+Ihor-Khomenko@users.noreply.github.com>
2021-11-21 22:12:45 +0200

    fixed url and added pattern for running tests

--- tests/petstore.test.ts
+++ tests/petstore.test.ts
@@ -3,9 +3,9 @@ import {strict as assert} from 'assert'
 
 describe("User can", function () {
     it('Receive pet by id', async function () {
-        const response = await got('https://localhost/v2/pet/1')
+        const response = await got('http://localhost:80/v2/pet/1')
         const body = JSON.parse(response.body)
 
-        assert(body.id == 1, 'Expected API to reurn pet with one')
+        assert(body.id == 1, `Expected API to reurn pet with id 1, but got ${body.id}`)
     })
 })
